a434b7ee1cce42d3f3686d1cbfcb8bac9327d02e
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-07-05 19:57:30 +0100

    checks if rb tree is valid

---
+++ 0x00-red_black_tree/1-rb_tree_is_valid.c
@@ -0,0 +1,44 @@
+#include "rb_trees.h"
+
+/**
+ * tree_validity - recursively valildates RB tree properties
+ * @tree: pointer to root of tree to validate
+ * @num_black: number of black nodes, passed by address
+ * Return: 1 if valid else 0
+ */
+int tree_validity(const rb_tree_t *tree)
+{
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (!tree->color)
+		return (0);
+
+	if (tree->color == RED)
+	{
+		if (tree->parent->color == tree->color)
+			return (0);
+		if (tree->color == tree->left->color || tree->color == tree->right->color)
+			return (0);
+	}
+
+	if (tree->left && !tree->right)
+		return (0);
+	if (!tree->left && tree->right)
+		return (0);
+
+	return (tree_validity(tree->right) && tree_validity(tree->left));
+
+
+}
+
+/**
+ * rb_tree_is_valid - checks if a binary tree is a valid Red-Black Tree
+ * @tree: pointer to root of tree to validate
+ * Return: 1 if valid else 0
+ */
+int rb_tree_is_valid(const rb_tree_t *tree)
+{
+	return (tree_validity(tree->right) && tree_validity(tree->left));
+}

6f7322dbcb0277a87375a221a44b804cf78785bf
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-07-05 15:23:29 +0100

    set memory with memset to check again

--- 0x00-red_black_tree/0-rb_tree_node.c
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -15,6 +15,7 @@ rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 	if (new_node == NULL)
 		return (NULL);
 
+	memset(new_node, 0, sizeof(*new_node));
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->color = color;

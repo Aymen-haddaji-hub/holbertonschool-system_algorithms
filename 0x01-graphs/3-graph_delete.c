2d84fdf38de5f97ee85e631e8b2c6e28c2f120f2
Aymen-Haddaji-hub <aymensystem7@gmail.com>
2021-08-17 22:19:49 +0100

    Task 3

---
+++ 0x01-graphs/3-graph_delete.c
@@ -0,0 +1,35 @@
+#include "graphs.h"
+
+/**
+ * graph_delete - A function that completely deletes a graph.
+ * @graph: a pointer to the graph to be deleted.
+ * Returns: Nothing (void)
+ */
+
+void graph_delete(graph_t *graph)
+{
+	vertex_t *new, *tmp;
+	edge_t *edge, *edge_tmp;
+
+	if (!graph)
+		return;
+
+	new = graph->vertices;
+	while (new)
+	{
+		free(new->content);
+		edge = new->edges;
+		while (edge)
+		{
+			edge_tmp = edge;
+			edge = edge->next;
+			free(edge_tmp);
+		}
+
+		tmp = new;
+		new = new->next;
+		free(tmp);
+	}
+
+	free(graph);
+}

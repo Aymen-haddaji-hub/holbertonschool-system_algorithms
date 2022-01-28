82006fc491f1745874230f00231b029b07132349
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-01-28 21:57:02 +0100

    task 2

--- 0x04-nary_trees/2-nary_tree_traverse.c
+++ 0x04-nary_trees/2-nary_tree_traverse.c
@@ -4,17 +4,21 @@
  * nary_tree_traverse - Inserts a new node in a N-ary tree
  * @root: Pointer to the root node
  * @action: Pointer to the function to be called for each node
- * @depth: Depth of the tree
+ * Return: the maximum depth of the tree
  */
-size_t nary_tree_traverse(nary_tree_t const *root, void (*action)(nary_tree_t const *node, size_t depth))
+size_t nary_tree_traverse(nary_tree_t const *root,
+		void (*action)(nary_tree_t const *node, size_t depth))
 {
+	size_t max_depth = 0;
 	size_t depth = 0;
 
 	if (!root)
 		return (0);
+	if (root->children)
+		max_depth = nary_tree_traverse(root->children, action);
+	depth = max_depth + 1;
 	action(root, depth);
-	depth++;
-	for (nary_tree_t const *node = root->children; node; node = node->next)
-		depth += nary_tree_traverse(node, action);
-	return (depth);
+	if (root->next)
+		max_depth = nary_tree_traverse(root->next, action);
+	return (depth > max_depth ? depth : max_depth);
 }
